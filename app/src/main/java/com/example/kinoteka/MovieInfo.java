4225f9a68d7c576f5b3dde4c740e512dca779990
AlOnEaKaALyOnA <sergij1230@gmail.com>
2023-05-28 01:52:47 +0700

    commу

---
+++ app/src/main/java/com/example/kinoteka/MovieInfo.java
@@ -0,0 +1,47 @@
+package com.example.kinoteka;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MovieInfo extends AppCompatActivity {
+
+    private Button back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_info);
+
+        back = findViewById(R.id.BACK);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backBtn = new Intent(MovieInfo.this, HomeActivity.class);
+                startActivity(backBtn);
+            }
+        });
+
+        ConstraintLayout courseBg = findViewById(R.id.MovieInfoBg);
+        ImageView movieImage = findViewById(R.id.MovieInfoImage);
+        TextView movieTitle = findViewById(R.id.MoviePageTitle);
+        TextView movieText = findViewById(R.id.moviePageText);
+        TextView movieLevel = findViewById(R.id.moviePageLevel);
+        TextView movieDate = findViewById(R.id.moviePageDate);
+
+        courseBg.setBackgroundColor(getIntent().getIntExtra("courseBg", 0));
+        movieImage.setImageResource(getIntent().getIntExtra("movieImage",0));
+        movieTitle.setText(getIntent().getStringExtra("movieTitle"));
+        movieText.setText(getIntent().getStringExtra("movieText"));
+        movieLevel.setText(getIntent().getStringExtra("movieLevel"));
+        movieDate.setText(getIntent().getStringExtra("movieDate"));
+    }
+}
